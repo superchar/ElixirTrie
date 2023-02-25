aa3cc4d402d8159cc94c846372413ffcab947f3b
Vladyslav Lobyntsev <vlobyntsev@deloitte.com>
2023-02-25 14:29:26 +0300

    Proper formatting

--- lib/elixir_trie.ex
+++ lib/elixir_trie.ex
@@ -3,20 +3,19 @@ defmodule TrieNode do
 end
 
 defmodule Trie do
-
   def put(node, word) do
-    put_recursive(node, word |> String.to_charlist)
+    put_recursive(node, word |> String.to_charlist())
   end
 
   def contains(node, word) do
-    case node |> find_node(word |> String.to_charlist) do
+    case node |> find_node(word |> String.to_charlist()) do
       %TrieNode{is_completed: is_completed} -> is_completed
       nil -> false
     end
   end
 
   def contains_prefix(node, word) do
-    case node |> find_node(word |> String.to_charlist) do
+    case node |> find_node(word |> String.to_charlist()) do
       %TrieNode{} -> true
       nil -> false
     end
@@ -24,14 +23,14 @@ defmodule Trie do
 
   defp put_recursive(node, [head | tail]) do
     child = node.children |> Map.put_new(head, %TrieNode{key: head}) |> Map.get(head)
-    %TrieNode {node| children: node.children |> Map.put(head, put_recursive(child, tail))}
+    %TrieNode{node | children: node.children |> Map.put(head, put_recursive(child, tail))}
   end
 
   defp put_recursive(node, []) do
     %TrieNode{node | is_completed: true}
   end
 
-  defp find_node(node,  [head | tail]) do
+  defp find_node(node, [head | tail]) do
     case node.children |> Map.fetch(head) do
       {:ok, child} -> find_node(child, tail)
       :error -> nil
@@ -41,7 +40,4 @@ defmodule Trie do
   defp find_node(node, []) do
     node
   end
-
 end
-
-
